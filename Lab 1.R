e6a96e0096080dcf6f7ac99dab3c62c88ee3f870
Michael-St-Jean <stjeanmi@msu.edu>
2020-01-20 19:15:18 -0500

    Second Commit

--- Lab 1.R
+++ Lab 1.R
@@ -21,8 +21,39 @@ v2 <- ggplot(data = data1, mapping = aes(x = class, y = drv)) +
 v3 <- ggplot(data = data1, mapping = aes(x = displ, y = hwy, color = class)) + 
   geom_point()
 
+# Exersize 2
+bankData <- read.csv("bank.csv")
+
+# Subset data to only 
+month_y <- as_tibble(bankData[,c("month","y")])
+
+successRateFrame <- month_y %>%
+  group_by(month) %>% 
+  summarize(Count = n(), Y = sum(y == "yes"), N = sum(y == "no"), Success_Rate = sum(y == "yes")/n()) 
+  
+successRatePlot <- ggplot(data = successRateFrame, mapping = aes(x= reorder(month, -Success_Rate) , y = Success_Rate)) + 
+  geom_bar(stat = "identity", color = "purple", fill = "purple") + 
+  labs(x = "Month", y = "Subscription Success Rate",title = "Subscription Success Rate by Month") + 
+  theme(plot.title = element_text(hjust = .5, size = 18)) + 
+  geom_text(
+    aes(label=round(Count)), 
+    size = 5, fontface = 2, color = 'black', 
+    hjust=0.5, vjust=-1) + 
+  ylim(0,.5)
 
 
+
+
+
+
+
+
+
+
+
+
+
+##########################################################################
 # Below Code is examples from the notes
 # ggplot example from notes
 gapminder
@@ -55,5 +86,53 @@ p + geom_point() +
 
 # Try it: The dollar sign changes the labels out of scientific and into a numerical amount in dollars. 
 # There are also other options such as number which just gives you the number scale and several others which make a similar trasnformation with slightly different outcomes.
+
+p <- ggplot(data = gapminder,
+            mapping = aes(x = gdpPercap, y = lifeExp, color = 'yellow'))
+p + geom_point() + scale_x_log10()
+# Try it : When running above code, it gives you a legend labeled yellow, but the dots are red, interesting
+# Try it : Mapping in the graph is determined by data in color 
+# If you put anything in aes() it is treated as data which doesn't make sense
+
+# Correct way to do it
+p <- ggplot(data = gapminder,
+            mapping = aes(x = gdpPercap, y = lifeExp))
+p + geom_point() + geom_smooth(color = "orange", se = FALSE, size = 8, method = "lm") + scale_x_log10()
+# Try it: color changes the line to orange, size changes the thickness of the line, se determines whether to include confidence interval, method determines smoothing function to use
+
+p + geom_point(alpha = 0.3) +  # alpha makes the points more standard
+  geom_smooth(method = "gam") +
+  scale_x_log10(labels = scales::dollar) +
+  labs(x = "GDP Per Capita", y = "Life Expectancy in Years",
+       title = "Economic Growth and Life Expectancy",
+       subtitle = "Data Points are country-years",
+       caption = "Source: Gapminder")
+
+library(scales)
+p <- ggplot(data = gapminder,
+            mapping = aes(x = gdpPercap, y = lifeExp, color = continent, fill = continent))
+p + geom_point()
+p + geom_point() + scale_x_log10(labels = dollar)
+p + geom_point() + scale_x_log10(labels = dollar) + geom_smooth()
+
+# Try it: the fill argument changes the fill of each point. Also, the match of colors and error bands makes this visualization a little noisy
+
+p <- ggplot(data = gapminder,
+            mapping = aes(x = gdpPercap, y = lifeExp))
+p + geom_point(mapping = aes(color = continent)) + geom_smooth() + scale_x_log10()
+# Try it: There is only one smooth line rather and one per continent because the color is determined 
+# in geom_point rather than in ggplot. This keeps the data together, and doesn't create essentially 
+# a pool of data per continent. 
+
+p <- ggplot(data = gapminder,
+            mapping = aes(x = gdpPercap, y = lifeExp))
+p + geom_point(mapping = aes(color = continent)) +
+  geom_smooth(mapping = aes(color = continent, fill = continent)) +
+  scale_x_log10() +
+  geom_smooth(mapping = aes(color = continent), method = "gam")
+# Try it: What is bad about the graph is there is multiple curves for each continent. 
+# For this reason we should set aes at the top rather than at each one as it will double count things
+
+
 
 
