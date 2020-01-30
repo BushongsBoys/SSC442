9ac4db4b8eb494252290128033fcd8a067a9587a
Jacob Loukota <jacobloukota@gmail.com>
2020-01-29 21:21:53 -0500

    finished excercize 2

--- Lab 2/lab2.r
+++ Lab 2/lab2.r
@@ -11,6 +11,7 @@ ameslist <- read.table("https://msudataanalytics.github.io/SSC442/Labs/data/ames
 # Remove any non-integer variables
 amesnums <- Filter(is.numeric, ameslist)
 library(dplyr)
+library(fastDummies)
 
 # Create a vector of the names of variables we do not have a reasonable intuition for
 drops <- c("MSSubClass", "MasVnrArea", "BsmtFinSF1", "BsmtFinSF2", "BsmntUnfSF", "LowQualFinSF", "X3SsnPorch", "MiscVal")
@@ -52,16 +53,41 @@ unlist(Ames[match(max(resid), resid),])
 
 
 #1 
-GarageTemp = model.matrix( ~ ameslist$GarageType - 1)
-ameslist <- cbind(ameslist, GarageTemp)
+ameslist <- fastDummies::dummy_cols(ameslist, select_columns = c("GarageType"))
+ameslist$GarageOutside <- ifelse(ameslist$GarageType_Detchd == 1 | ameslist$GarageType_CarPort == 1, 1, 0)
 
+lmGarage.fit = lm(SalePrice ~ ameslist$GarageOutside)
+#as you can see having an outdoor garage is associated with a drop in the 
+#sale price of 72859
 
 #2 
-
 # Create linear regression model with all variables in Ames as predictor variables
 sp_model <- lm(SalePrice ~ ., data = Ames)
 summary(sp_model)
 
+#4
+inter_model1 <- lm(SalePrice ~ OverallQual+ GrLivArea + OverallQual*GrLivArea, data=Ames)
+summary(inter_model1)
+#statistically significant interaction between quality and above ground living area
+inter_model2 <- lm(SalePrice ~ GrLivArea + OverallQual + YearBuilt+ YearRemodAdd + YearBuilt*YearRemodAdd, data=Ames)
+summary(inter_model2)
+#no statistically significant interaction between yearbuilt and year remodeled
+inter_model3 <- lm(SalePrice ~ GrLivArea + OverallQual + LotArea + OverallQual*LotArea, data=Ames)
+summary(inter_model3)
+#interestingly not a significant interaction between lot area and quality
+inter_model4 <- lm(SalePrice ~ GrLivArea + OverallQual + LotArea + LotFrontage +  LotFrontage*LotArea, data=Ames)
+summary(inter_model4)
+#statistically significant but small interaction between lot area and lot frontage.
+#it may make sense to include the 2 significant interactions in the final model
 
+#5
+transformer_model1 <- lm(log(SalePrice) ~ ., data = Ames)
+summary(transformer_model1)
+#using log of sale price allows us to see percent change effects of the variables
 
+transformer_model2 <- lm(SalePrice ~ log(GrLivArea), data = Ames)
+summary(transformer_model2)
+#allows you to see the effects of a percent change in GrLivArea
 
+transformer_model3 <- lm(SalePrice ~ GrLivArea + LotArea + LotArea*LotArea, data = Ames)
+summary(transformer_model2)
